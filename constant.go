44aa18deacb3c70518b02432b32e2d8c4a4abaa0
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2022-03-25 11:01:24 +0700

    Constant

---
+++ constant.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	const firsName string = "Rezki"
+	const lastName = "Ananda"
+	const value = 25
+
+	const (
+		address = "Kediri"
+		country = "Indonesia"
+	)
+
+	fmt.Println(firsName)
+	fmt.Println(lastName)
+}
