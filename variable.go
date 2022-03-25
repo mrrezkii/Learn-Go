df4ca116e96a22a022e3dc89d3918efd835e9e11
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2022-03-25 08:46:42 +0700

    Variable

---
+++ variable.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Muhammad Rezki Ananda"
+	fmt.Println(name)
+
+	name = "Rezki Ananda"
+	fmt.Println(name)
+
+	var friendName = "Denny"
+	fmt.Println(friendName)
+
+	var age int8 = 30
+	fmt.Println(age)
+
+	country := "Indonesia"
+	fmt.Println(country)
+
+	var (
+		firstName = "Muhammad Rezki"
+		lastName  = "Ananda"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
