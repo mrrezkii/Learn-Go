0bc0f3cc92d8aa0e7e1d955ed8faf433e51717de
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2022-03-27 08:43:34 +0700

    Recursive function

---
+++ function-recursive.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func factorialLoop(value int) int {
+	result := 1
+	for i := value; i > 0; i-- {
+		result *= i
+	}
+	return result
+}
+
+func factorialRecursive(value int) int {
+	if value == 1 {
+		return 1
+	} else {
+		return value * factorialRecursive(value-1)
+	}
+}
+
+func main() {
+	loop1 := factorialLoop(5)
+	loop2 := factorialRecursive(5)
+	fmt.Println(loop1)
+	fmt.Println(loop2)
+}
