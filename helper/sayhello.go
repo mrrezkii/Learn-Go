514c4d2e133fb9f7f1c2ada4674c96f8a02a63f9
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2022-04-02 11:10:08 +0700

    Access modifier

--- helper/sayhello.go
+++ helper/sayhello.go
@@ -2,6 +2,14 @@ package helper
 
 import "fmt"
 
+var version = "1.0"
+var Application = "Belajar"
+
+// private
+func sayHello(name string) {
+	fmt.Println("Hello", name)
+}
+
 func SayHello(name string) {
 	fmt.Println("Hello", name)
 }
--- import.go
+++ import.go
@@ -4,4 +4,5 @@ import "belajar-golang-dasar/helper"
 
 func main() {
 	helper.SayHello("Rezki")
+	//helper.sayHello("Nando") // private dari package lain, tidak bisa di ambil
 }
